5aeef9478ce8666aa647875085c44909f9a76673
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 01:17:28 -0700

    Annotating, fixing issues with packages

--- index.ts
+++ index.ts
@@ -3,14 +3,16 @@ import greaterThan from "@unction/greaterthan";
 import append from "@unction/append";
 import reduceWithValueKey from "@unction/reducewithvaluekey";
 import fresh from "@unction/fresh";
-export default function takeFirst (count) {
-  return function takeFirstCount (orderedList) {
-    return reduceWithValueKey((accumulated) => (value) => (index) => {
+import {OrderedEnumerableType} from "./types";
+
+export default function takeFirst<V> (count: number) {
+  return function takeFirstCount (orderedEnumerable: OrderedEnumerableType<V>) {
+    return reduceWithValueKey((accumulated: OrderedEnumerableType<V>) => (value: V) => (index: number) => {
       if (greaterThan(index)(count - 1)) {
         return accumulated;
       }
 
       return append(value)(accumulated);
-    })(fresh(orderedList))(orderedList);
+    })(fresh(orderedEnumerable))(orderedEnumerable);
   };
 }
