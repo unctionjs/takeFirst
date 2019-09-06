1ac35d0452a9da0cae2cdef48461daa6c9b87fd9
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:53 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-magic-numbers */
+import takeFirst from "./index.ts";
+
+test(() => {
+  expect(takeFirst(2)(["a", "b", "c"])).toEqual(["a", "b"]);
+});
+
+test(() => {
+  expect(takeFirst(1)(["a", "b", "c"])).toEqual(["a"]);
+});
+
+test(() => {
+  expect(takeFirst(2)("abc")).toEqual("ab");
+});
+
+test(() => {
+  expect(takeFirst(1)("abc")).toEqual("a");
+});
