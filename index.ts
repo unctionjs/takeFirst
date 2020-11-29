084638810a062fec46be172565b89c7c3d38b6ba
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-28 18:50:21 -0800

    Denormalizing some types to make things a little bit easier

--- index.ts
+++ index.ts
@@ -3,11 +3,10 @@ import greaterThan from "@unction/greaterthan";
 import append from "@unction/append";
 import reduceWithValueKey from "@unction/reducewithvaluekey";
 import fresh from "@unction/fresh";
-import {OrderedEnumerableType} from "./types";
 
 export default function takeFirst<V> (count: number) {
-  return function takeFirstCount (orderedEnumerable: OrderedEnumerableType<V>) {
-    return reduceWithValueKey((accumulated: OrderedEnumerableType<V>) => (value: V) => (index: number) => {
+  return function takeFirstCount (orderedEnumerable: Array<V> | string) {
+    return reduceWithValueKey((accumulated: Array<V> | string) => (value: V) => (index: number) => {
       if (greaterThan(index)(count - 1)) {
         return accumulated;
       }
